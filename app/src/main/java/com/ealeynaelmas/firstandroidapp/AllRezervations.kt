98dc1b501abd19bd35e9414b99eac0ccc9cd8ffb
E-AleynaElmas <a.aleynaelmas@gmail.com>
2022-01-08 23:41:09 +0300

    New pages and rıules
    -Show all rezervation for X hotel
    -Rebooking page
    -Hotel query page

---
+++ app/src/main/java/com/ealeynaelmas/firstandroidapp/AllRezervations.kt
@@ -0,0 +1,11 @@
+package com.ealeynaelmas.firstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllRezervations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_rezervations)
+    }
+}
---
+++ app/src/main/java/com/ealeynaelmas/firstandroidapp/HotelLogin.kt
@@ -0,0 +1,11 @@
+package com.ealeynaelmas.firstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_login)
+    }
+}
